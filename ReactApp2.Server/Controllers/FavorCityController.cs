8259ec67bf40363255a88135ff1f140524290122
z9z8c7 <1290434330@qq.com>
2024-07-22 18:44:36 +1200

    New API about users' favorite cities.

---
+++ ReactApp2.Server/Controllers/FavorCityController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReactApp2.Server.Models;
+using ReactApp2.Server.Services;
+
+namespace ReactApp2.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FavorCityController : ControllerBase
+    {
+        // GET: api/<FavorCityController>
+        [HttpGet]
+        public ActionResult<List<FavorCity>> GetAll() =>
+            UserCityService.GetAll();
+
+        // GET api/<FavorCityController>/5
+        [HttpGet("{id}")]
+        public ActionResult<FavorCity> Get(int id)
+        {
+            var city = UserCityService.Get(id);
+            if (city == null)
+                return NotFound();
+            return city;
+        }
+
+        // POST api/<FavorCityController>
+        [HttpPost]
+        public IActionResult Post([FromBody] FavorCity newcity)
+        {
+            UserCityService.Add(newcity);
+            return CreatedAtAction(nameof(Get), new { userid = newcity.UserId }, newcity);
+        }
+
+        // PUT api/<FavorCityController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] FavorCity city)
+        {
+            if (id != city.Id)
+                return BadRequest();
+            var existingcity = UserCityService.Get(id);
+            if (existingcity is null)
+                return NotFound();
+            UserCityService.Update(city);
+            return NoContent();
+        }
+
+        // DELETE api/<FavorCityController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var city = UserCityService.Get(id);
+            if(city is null)
+                return NotFound();
+            UserCityService.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ ReactApp2.Server/Models/FavorCity.cs
@@ -0,0 +1,10 @@
+﻿namespace ReactApp2.Server.Models
+{
+    public class FavorCity
+    {
+        public long UserId { get; set; }
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
