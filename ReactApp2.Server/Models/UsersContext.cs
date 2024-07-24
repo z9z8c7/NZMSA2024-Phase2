849099ce9dabc34477836a480cc0468658760245
z9z8c7 <1290434330@qq.com>
2024-07-24 20:15:32 +1200

    New API Users for admins and users.

---
+++ ReactApp2.Server/Models/UsersContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReactApp2.Server.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
+
+        public DbSet<Users> Users { get; set; }
+    }
+}
